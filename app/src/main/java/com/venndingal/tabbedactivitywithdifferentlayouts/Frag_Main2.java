c8ab80906727f21cdb1e0cf98eb8a3b61e169fcc
Venn DIngal <venndingal@gmail.com>
2016-08-29 17:59:23 +0800

    Tabbed Activity with Multiple Views

---
+++ app/src/main/java/com/venndingal/tabbedactivitywithdifferentlayouts/Frag_Main2.java
@@ -0,0 +1,13 @@
+package com.venndingal.tabbedactivitywithdifferentlayouts;
+
+import android.widget.TextView;
+
+/**
+ * Created by keng on 29/08/16.
+ */
+public class Frag_Main2 {
+
+    public void main (TextView tv){
+        tv.setText("This is page 2");
+    }
+}
---
+++ app/src/main/java/com/venndingal/tabbedactivitywithdifferentlayouts/Frag_Main3.java
@@ -0,0 +1,13 @@
+package com.venndingal.tabbedactivitywithdifferentlayouts;
+
+import android.widget.TextView;
+
+/**
+ * Created by keng on 29/08/16.
+ */
+public class Frag_Main3 {
+
+    public void main (TextView tv){
+        tv.setText("This is page 3");
+    }
+}
